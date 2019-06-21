d0c08e80e4688b20bdea186fb17ac8e676e26a16
William <naylor.william@gmail.com>
2019-06-20 20:24:56 -0500

    initial commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,17 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+import { Route } from 'react-router';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Counter from './components/Counter';
+import FetchData from './components/FetchData';
+
+
+ const App = () => (
+  <Layout>
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+  </Layout>
+);
+export default hot(App);
